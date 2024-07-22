e2ce04bb746c494134f65aaa5a5a86a78f3093ae
sumit narkhede <56193755+sumit2304@users.noreply.github.com>
2024-07-22 21:39:14 +0530

    init

---
+++ bpe-mobileservices-model/src/main/java/com/cestasoft/mobileservices/model/dto/ModelDTO1.java
@@ -0,0 +1,13 @@
+package com.cestasoft.mobileservices.model.dto;
+
+public class ModelDTO1 {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
